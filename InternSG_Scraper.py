d9c6e7e6fe031754181bd4e712bdd8824f620089
Chan Zhe Xiang <zxchan.2021@scis.smu.edu.sg>
2023-07-29 15:31:09 +0800

    edit

--- InternSG_Scraper.py
+++ InternSG_Scraper.py
@@ -1,6 +1,35 @@
 import time
+from datetime import datetime
 import requests
 from bs4 import BeautifulSoup
+import os
+
+now = datetime.now()
+year = str(now.year)
+month = now.strftime("%B")
+day = str(now.day)
+filePath = f"./{year}/{month}/{day}_{month}_{year}_jobs.doc"
+os.makedirs(os.path.dirname(filePath), exist_ok=True)
+
+def find_rows(odd_or_even):
+    if odd_or_even == "even":
+        rows = soup.find_all("div", class_="ast-row list-even")
+    elif odd_or_even == "odd":
+        rows = soup.find_all("div", class_="ast-row list-odd")
+    
+    for row in rows:
+            post_date = row.find_all("span")[-1].contents[0]
+            if post_date != "Closed":
+                row = row.find_all("div")
+                company_name = row[0].contents[0]
+                title = row[1].contents[0].get_text()
+                link = row[1].contents[0].get("href")
+
+                print(f"\tName: {company_name}\n\tJob Title: {title}\n\tLink: {link}\n")
+                with open(filePath, "a") as jobFile:
+                     jobFile.write(f"\tName: {company_name}\n\tJob Title: {title}\n\tLink: {link}\n\n")
+
+            time.sleep(0.2)
 
 filters = ["software", "frontend", "backend"]
 for filter in filters:
@@ -9,33 +38,15 @@ for filter in filters:
 
         page = requests.get(url)
         soup = BeautifulSoup(page.text, 'html.parser')
-        print(f"{filter}, {page_num}")
+
+        print(f"{filter}, page: {page_num}")
+        with open(filePath, "a") as jobFile:
+            jobFile.write(f"{filter}, page: {page_num}\n")
 
         pages = soup.find_all("div", class_="ast-row ast-pagination-square")
 
+        find_rows("odd")
+        find_rows("even")
+
         if not pages:
             break
-        else:
-            even_row = soup.find_all("div", class_="ast-row list-even")
-            for row in even_row:
-                post_date = row.find_all("span")[-1].contents[0]
-                if post_date != "Closed":
-                    row = row.find_all("div")
-                    company_name = row[0].contents[0]
-                    title = row[1].contents[0].get_text()
-
-                    print(f"\t{company_name}, {title}")
-
-                time.sleep(0.1)
-
-            odd_row = soup.find_all("div", class_="ast-row list-odd")
-            for row in odd_row:
-                post_date = row.find_all("span")[-1].contents[0]
-                if post_date != "Closed":
-                    row = row.find_all("div")
-                    company_name = row[0].contents[0]
-                    title = row[1].contents[0].get_text()
-
-                    print(f"\t{company_name}, {title}")
-
-                time.sleep(0.1)
